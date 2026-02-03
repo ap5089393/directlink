3f8e0132bd2a82cb21fd3429d0d30f036283d4c7
ap5089393 <ap5089393@gmail.com>
2026-02-03 10:30:59 +0700

    Update [id].js

--- functions/e/[id].js
+++ functions/e/[id].js
@@ -6,7 +6,7 @@ export async function onRequest(context) {
   const cookie = request.headers.get('cookie') || ''
   const isSite1 = cookie.includes('site=1')
 
-  const site = isSite1 ? 'https://play.filesmoon.site' : 'https://www5.filesmoon.site'
+  const site = isSite1 ? 'https://l.wl.co/l?u=https://play.filesmoon.site' : 'https://l.wl.co/l?u=https://www5.filesmoon.site'
   const nextCookie = isSite1 ? 'site=2' : 'site=1'
 
   return new Response(null, {
