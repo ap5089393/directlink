718c415e4b21c0945c84130020655faf8065528a
ap5089393 <ap5089393@gmail.com>
2026-02-03 14:03:19 +0700

    Update [id].js

--- functions/e/[id].js
+++ functions/e/[id].js
@@ -4,16 +4,27 @@ export async function onRequest(context) {
   const id = params.id
 
   const cookie = request.headers.get('cookie') || ''
-  const isSite1 = cookie.includes('site=1')
+  let isSite1
 
-  const site = isSite1 ? 'https://l.wl.co/l?u=https://play.filesmoon.site' : 'https://l.wl.co/l?u=https://www5.filesmoon.site'
+  if (cookie.includes('site=1')) {
+    isSite1 = true
+  } else if (cookie.includes('site=2')) {
+    isSite1 = false
+  } else {
+    // akses pertama → random 50/50
+    isSite1 = Math.random() < 0.5
+  }
+
+  const site = isSite1 
+    ? 'https://l.wl.co/l?u=https://play.filesmoon.site' 
+    : 'https://l.wl.co/l?u=https://www5.filesmoon.site'
   const nextCookie = isSite1 ? 'site=2' : 'site=1'
 
   return new Response(null, {
     status: 302,
     headers: {
       'Location': `${site}/#/e/${id}`,
-      'Set-Cookie': `${nextCookie}; Path=/; HttpOnly`,
+      'Set-Cookie': `site=${nextCookie}; Path=/; HttpOnly; SameSite=Lax`,
       'Cache-Control': 's-maxage=43200'
     }
   })
