e88215df42428a1c6943f3186f15963392a7635a
ap5089393 <ap5089393@gmail.com>
2026-02-03 10:21:21 +0700

    Update [id].js

--- functions/e/[id].js
+++ functions/e/[id].js
@@ -1,33 +1,20 @@
+// functions/e/[id].js
 export async function onRequest(context) {
   const { params, request } = context
-  const id = params.id || 'default'
+  const id = params.id
 
-  // baca cookie
   const cookie = request.headers.get('cookie') || ''
   const isSite1 = cookie.includes('site=1')
 
-  // pilih site bergantian
   const site = isSite1 ? 'https://play.filesmoon.site' : 'https://www5.filesmoon.site'
   const nextCookie = isSite1 ? 'site=2' : 'site=1'
 
-  // buat halaman HTML redirect tanpa JS
-  const html = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="refresh" content="0; url=${site}/#/e/${id}">
-  <title>Redirecting...</title>
-</head>
-<body>
-  Redirecting to <a href="${site}/#/e/${id}">${site}/#/e/${id}</a>...
-</body>
-</html>`
-
-  return new Response(html, {
+  return new Response(null, {
+    status: 302,
     headers: {
-      'content-type': 'text/html;charset=UTF-8',
+      'Location': `${site}/#/e/${id}`,
       'Set-Cookie': `${nextCookie}; Path=/; HttpOnly`,
-      'Cache-Control': 's-maxage=43200' // cache 12 jam
+      'Cache-Control': 's-maxage=43200'
     }
   })
 }
