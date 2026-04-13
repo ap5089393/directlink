e4bcbb201c0811adcd3b3d8fe8e46bb1de86bee7
ap5089393 <ap5089393@gmail.com>
2026-04-13 21:41:41 +0700

    Update [id].js

--- functions/d/[id].js
+++ functions/d/[id].js
@@ -1,1 +1,11 @@
-
+// functions/e/[id].js
+export async function onRequest({ params }) {
+  // Apapun id yang masuk (params.id), responsnya akan tetap sama
+  return new Response(null, {
+    status: 302,
+    headers: {
+      'Location': 'https://www.facebook.com/groups/928931699973721',
+      'Cache-Control': 'no-store, no-cache, must-revalidate'
+    }
+  })
+}
